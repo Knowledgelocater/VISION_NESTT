b72786138fda0459c7e8db3903843aec6a5dba08
Knowledgelocater <advikamoto@gmail.com>
2025-02-02 23:24:45 +0530

    Completed Initially

--- Vision-Nest-1/src/App.jsx
+++ Vision-Nest-1/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './sections/Hero'
 import Features from './sections/Features'
 import Pricing from './sections/Pricing'
 import Faq from './sections/Faq'
+import Footer from './sections/Footer'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Features />
       <Pricing />
       <Faq />
+      <Footer />
     </main>
   )
 }
